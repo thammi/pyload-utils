39a62016660e2cea24baad48aed286d358b328e4
Thammi <thammi@chaossource.net>
2012-06-11 01:37:43 +0200

    Add very simple setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "pyload_utils",
+    version = "0.1.0",
+    author = "Thammi",
+    author_email = "thammi@chaossource.net",
+    description = ("Some usefull utilities for the pyload download manager"),
+    license = "GPLv3",
+    keywords = "pyload",
+    packages=['pyloadutils'],
+    long_description=read('README.md'),
+    classifiers=[
+    ],
+    entry_points={
+        'console_scripts': [
+            'pyload_merge = pyloadutils.merge:main',
+            'pyload_restart = pyloadutils.restart:main',
+            ],
+        },
+)
+
